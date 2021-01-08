58e0a16f8f5fce3321ebad3abecf1bd580b1fc5c
Ignacio Larreta <ignacio@larreta.com.ar>
2021-01-08 00:20:51 -0300

    cosas

--- src/app/components/home/components/home/home.component.ts
+++ src/app/components/home/components/home/home.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  show: boolean;
+
+  constructor() {
+    this.show = true;
+  }
+
 }
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -5,14 +5,16 @@ import {HomeComponent} from './components/home/home.component';
 import {CalendarModule} from '../../library/calendar/calendar.module';
 import {CategoriesComponent} from './components/home/categories/categories.component';
 import {MatListModule} from '@angular/material/list';
+import {TranslateModule} from '../../library/translate/translate.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    CalendarModule,
-    MatListModule
-  ],
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        CalendarModule,
+        MatListModule,
+        TranslateModule
+    ],
   declarations: [
     HomeComponent,
     CategoriesComponent
